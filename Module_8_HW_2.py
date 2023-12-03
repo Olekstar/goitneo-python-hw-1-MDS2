60e8513034b9d81eb2dcc461737af358161a0e23
Olekstar <ostarovit@gmail.com>
2023-12-03 23:51:49 +0200

    ver 1.1 - added comments

--- Module_8_HW_2.py
+++ Module_8_HW_2.py
@@ -1,39 +1,45 @@
 def parse_input(user_input):
+    # Розділення введеного рядка на команду та аргументи
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, args
 
 def add_contact(args, contacts):
+    # Додавання контакту
     if len(args) == 2:
         name, phone = args
         contacts[name] = phone
-        return "Contact added."
+        return "Контакт додано."
     else:
-        return "Invalid command. Usage: add <name> <phone>"
+        return "Невірна команда. Використовуйте: add <ім'я> <телефон>"
 
 def list_contacts(contacts):
+    # Виведення усіх контактів
     if not contacts:
-        return "No contacts found."
+        return "Контактів не знайдено."
     else:
         return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
 def save_contacts(contacts, filename="contacts.txt"):
+    # Збереження контактів у файл
     with open(filename, "w") as file:
         for name, phone in contacts.items():
             file.write(f"{name}:{phone}\n")
 
 def change_contact(args, contacts):
+    # Зміна номера телефону для заданого імені
     if len(args) == 2:
         name, new_phone = args
         if name in contacts:
             contacts[name] = new_phone
-            return f"Phone number for {name} changed to {new_phone}."
+            return f"Номер телефону для {name} змінено на {new_phone}."
         else:
-            return f"Contact with name {name} not found."
+            return f"Контакт з ім'ям {name} не знайдено."
     else:
-        return "Invalid command. Usage: change <name> <new_phone>"
+        return "Невірна команда. Використовуйте: change <ім'я> <новий_телефон>"
 
 def load_contacts(filename="contacts.txt"):
+    # Завантаження контактів з файлу
     contacts = {}
     try:
         with open(filename, "r") as file:
@@ -45,19 +51,20 @@ def load_contacts(filename="contacts.txt"):
     return contacts
 
 def main():
+    # Основна функція бота
     contacts = load_contacts()
-    print("Welcome to the assistant bot!")
+    print("Ласкаво просимо до бота-асистента!")
     
     while True:
-        user_input = input("Enter a command: ")
+        user_input = input("Введіть команду: ")
         command, args = parse_input(user_input)
 
         if command in ["close", "exit"]:
             save_contacts(contacts)
-            print("Good bye!")
+            print("До побачення!")
             break
         elif command == "hello":
-            print("How can I help you?")
+            print("Як я можу вам допомогти?")
         elif command == "add":
             print(add_contact(args, contacts))
         elif command == "all":
@@ -65,7 +72,7 @@ def main():
         elif command == "change":
             print(change_contact(args, contacts))
         else:
-            print("Invalid command.")
+            print("Невірна команда.")
 
 if __name__ == "__main__":
     main()
